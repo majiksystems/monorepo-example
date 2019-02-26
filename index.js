50948d498b23c500eddc0d0607e296b5b463d9d7
Ben Kiefer <bwkiefer@majik.io>
2019-02-26 15:53:21 -0500

    feat: adds functionality to create core server

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+let name = process.argv[2];
+let time = process.argv[3];
+let uptime = process.argv[4];
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`Hello, ${name}! The time is ${time}. Time spent working is ${uptime} hours and minutes!\n`);
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+
+  var url = 'http://localhost:3000';
+  var start = (process.platform == 'darwin'? 'open': process.platform == 'win32'? 'start': 'xdg-open');
+  require('child_process').exec(start + ' ' + url);
+});
