072fec3b7e0dca1ea133be653110dc89415c4327
Ben Kiefer <bwkiefer@majik.io>
2019-02-28 11:15:32 -0500

    Undo: remove last commit containing breaking special feature

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ const port = 3000;
 let name = process.argv[2];
 let time = process.argv[3];
 let uptime = process.argv[4];
-let cwd = process.argv[5].replace('/', '->');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(
-    `Hello, ${name}! The time is ${time}. Time spent working is ${uptime} hours and minutes!\nCWD: ${cwd}\n`);
+    `Hello, ${name}! The time is ${time}. Time spent working is ${uptime} hours and minutes!\n`);
 });
 
 server.listen(port, () => {
