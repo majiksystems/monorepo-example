d3ef4405133dca84358b3dfed367f17d4cb1b1d7
Ben Kiefer <bwkiefer@majik.io>
2019-02-28 11:08:50 -0500

    feat: displays cwd in a very "special way"

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const port = 3000;
 let name = process.argv[2];
 let time = process.argv[3];
 let uptime = process.argv[4];
+let cwd = process.argv[5].replace('/', '->');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(
-    `Hello, ${name}! The time is ${time}. Time spent working is ${uptime} hours and minutes!\n`);
+    `Hello, ${name}! The time is ${time}. Time spent working is ${uptime} hours and minutes!\nCWD: ${cwd}\n`);
 });
 
 server.listen(port, () => {
--- packages/deployment/start.sh
+++ packages/deployment/start.sh
@@ -2,4 +2,4 @@
 
 tag=${1:-"latest"}
 
-docker run -p 3000:3000 -it majik:$tag /bin/sh -c "node /index.js $(whoami) $(date '+%H:%M') $(uptime | cut -d' ' -f5 | tr -d ',')"
+docker run -p 3000:3000 -it majik:$tag /bin/sh -c "node /index.js $(whoami) $(date '+%H:%M') $(uptime | cut -d' ' -f5 | tr -d ',') $(pwd)"
